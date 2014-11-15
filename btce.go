48f23d5fa3b8eab2a64d4da8778319df96229955
Toni Korpela <toni@mussukka.org>
2014-11-15 22:08:23 +0200

    fix some syntax errors

--- btce.go
+++ btce.go
@@ -5,7 +5,7 @@ type btce struct {
 	Private *btcePrivate
 }
 
-func New(public string, secret string) {
+func New(public string, secret string) *btce {
 	pub := NewPublic();
 	priv := NewPrivate(public, secret);
 	return &btce{pub, priv};
@@ -15,12 +15,12 @@ func (this *btce) GetInfo() (*info, error) {
 	return this.Private.GetInfo();
 }
 
-func (this *btce) TransHistory(From int, Count int, FromID int, EndID, int, Order string, Since string, End string) (*transHistory, error) {
+func (this *btce) TransHistory(From int, Count int, FromID int, EndID int, Order string, Since string, End string) (*transHistory, error) {
 	return this.Private.TransHistory(From, Count, FromID, EndID, Order, Since, End);
 }
 
-func (this *btce) TradeHistory(From int, Count int, FromID int, EndID, int, Order string, Since string, End string) (*tradeHistory, error) {
-	return this.Private.TradeHistory(From, Count, FromID, EndID, Order, Since, End);
+func (this *btce) TradeHistory(From int, Count int, FromID int, EndID int, Order string, Since string, End string, Pair string) (*tradeHistory, error) {
+	return this.Private.TradeHistory(From, Count, FromID, EndID, Order, Since, End, Pair);
 }
 
 func (this *btce) ActiveOrders(Pair string) (*activeOrders, error) {
@@ -35,7 +35,7 @@ func (this *btce) CancelOrder(OrderID int) (*cancelOrder, error) {
 	return this.Private.CancelOrder(OrderID);
 }
 
-func (this *btce) Fee(Pair string) (*fee, Error) {
+func (this *btce) Fee(Pair string) (*fee, error) {
 	return this.Public.Fee(Pair);
 }
 
@@ -47,6 +47,6 @@ func (this *btce) Trades(Pair string) (*trades, error) {
 	return this.Public.Trades(Pair);
 }
 
-func (this *btce) Depth(Pair string) (*trades, error) {
+func (this *btce) Depth(Pair string) (*depth, error) {
 	return this.Public.Depth(Pair);
 }
