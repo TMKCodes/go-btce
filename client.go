9b9f2dff8581fca098e80a6b2b4acf1fbd3ecda2
Toni Korpela <toni@mussukka.org>
2014-11-15 15:50:35 +0200

    add possibility to disable hmac authentication in the client

---
+++ client.go
@@ -0,0 +1,51 @@
+package btce
+
+import (
+	"bytes"
+	"strings"
+	"net/url"
+	"net/http"
+	"crypto/tls"
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
+)
+
+const (
+	TRADE_API_ENDPOINT = "https://btc-e.com/tapi"
+)
+
+type clientTLS struct {
+	public string
+	secret string
+	hmac bool
+}
+
+func NewClient(public string, secret string, hmac bool) *clientTLS) {
+	return &clientTLS{public, secret, hmac};
+}
+
+
+func (this *clientTLS) HMAC_SHA512(secret string, message string) string {
+	hash := hmac.New(sha512.New, []byte(secret));
+	hash.Write([]byte(message));
+	return strings.ToLower(hex.EncodeToString(hash.Sum(nil)));
+}
+
+func (this *clientTLS) Request(data url.Values) (*http.Response, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true },
+	};
+	httpClient := &http.Client{Transport: tr};
+	params := data.Encode();
+	request, err := http.NewRequest("POST", TRADE_API_ENDPOINT, bytes.NewBufferString(params));
+	if err != nil {
+		return nil, err;
+	}
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded");
+	if this.hmac == true {
+		request.Header.Set("Key", this.public);
+		request.Header.Set("Sign", this.HMAC_SHA512(this.secret, params));
+	}
+	return httpClient.Do(request);
+}
