5baa3e3b5c4a782f7bd40e42d23068419ff39a6c
Toni Korpela <toni@mussukka.org>
2014-11-17 01:01:24 +0200

    ..

---
+++ examples/trades.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/TMKCodes/go-btce"
+)
+
+const (
+	KEY = "M7Z2I8HR-07RJRR6L-PO7DDGGX-LCJ7VW3B-NX7PCSET"
+	SECRET = "4a720e1de1ba08bec388209996df8141e48813c57dbc12425985eebf3e8e3b87"
+)
+
+func main() {
+	btce := btce.New(KEY, SECRET);
+
+	Trades, err := btce.Trades("btc_usd");
+	if err != nil {
+		fmt.Printf("Trades: %v\n", err);
+	} else {
+		fmt.Printf("Trades: %#v\n", Trades);
+	}
+}
